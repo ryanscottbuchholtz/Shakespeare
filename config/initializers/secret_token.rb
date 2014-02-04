0be128a7012e9bf1f7075b8a57f84aeb7e13a7b4
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-02-04 09:05:36 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shakespeare::Application.config.secret_key_base = '2a334d012e9e4edb752d4a27c94436c7f6377ae6b425f397dbfc7b0b6e647a5237ee59f86f2d39cbba217b920db705c17a956f2989bfcd88878fed07f7496ee0'
